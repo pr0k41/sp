379717b7961670163f8881ef8baacfd103517831
Nilay <114640991+pr0k41@users.noreply.github.com>
2024-12-29 22:31:08 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,95 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+
+/**
+ * Search function
+ */
+
+const searchInput = document.querySelector("#searchbar > input")
+const searchButton = document.querySelector("#searchbar > button")
+
+const lookup = {}
+const engine = "google"
+const engineUrls = {
+  deepl: "https://www.deepl.com/translator#-/-/{query}",
+  duckduckgo: "https://duckduckgo.com/?q={query}",
+  ecosia: "https://www.ecosia.org/search?q={query}",
+  google: "https://www.google.com/search?q={query}",
+  startpage: "https://www.startpage.com/search?q={query}",
+  youtube: "https://www.youtube.com/results?q={query}",
+}
+
+const isWebUrl = value => {
+  try {
+    const url = new URL(value)
+    return url.protocol === "http:" || url.protocol === "https:"
+  } catch {
+    return false
+  }
+}
+
+const getTargetUrl = value => {
+  if (isWebUrl(value)) return value
+  if (lookup[value]) return lookup[value]
+  const url = engineUrls[engine] ?? engine
+  return url.replace("{query}", value)
+}
+
+const search = () => {
+  const value = searchInput.value
+  const targetUrl = getTargetUrl(value)
+  window.open(targetUrl, "_self")
+}
+
+searchInput.onkeyup = event => event.key === "Enter" && search()
+searchButton.onclick = search
+
+/**
+ * inject bookmarks into html
+ */
+
+const bookmarks = [{"id":"hFgoAt7HXKOfPrp7","label":"System","bookmarks":[{"id":"4SHC38vxhC1srbDy","label":"cal","url":"https://calendar.google.com"},{"id":"0kg7tmyfxJlpdG3V","label":"pages","url":"https://notion.so/"}]},{"id":"vKyZdeS7B61NCTcE","label":"Media","bookmarks":[{"id":"koHKstkkT0fkIsUh","label":"yt","url":"https://youtube.com/"},{"id":"74xtTO7hAV8A5qc4","label":"yt mus","url":"https://music.youtube.com/"}]}]
+
+const createGroupContainer = () => {
+  const container = document.createElement("div")
+  container.className = "bookmark-group"
+  return container
+}
+
+const createGroupTitle = title => {
+  const h2 = document.createElement("h2")
+  h2.innerHTML = title
+  return h2
+}
+
+const createBookmark = ({ label, url }) => {
+  const li = document.createElement("li")
+  const a = document.createElement("a")
+  a.href = url
+  a.innerHTML = label
+  li.append(a)
+  return li
+}
+
+const createBookmarkList = bookmarks => {
+  const ul = document.createElement("ul")
+  bookmarks.map(createBookmark).forEach(li => ul.append(li))
+  return ul
+}
+
+const createGroup = ({ label, bookmarks }) => {
+  const container = createGroupContainer()
+  const title = createGroupTitle(label)
+  const bookmarkList = createBookmarkList(bookmarks)
+  container.append(title)
+  container.append(bookmarkList)
+  return container
+}
+
+const injectBookmarks = () => {
+  const bookmarksContainer = document.getElementById("bookmarks")
+  bookmarksContainer.append()
+  bookmarks.map(createGroup).forEach(group => bookmarksContainer.append(group))
+}
+
+injectBookmarks()
